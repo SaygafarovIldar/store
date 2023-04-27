bac83b7fd52aaa2f05e8a3af5b93bb60f5ed2cbe
ildar <sayildar17@gmail.com>
2023-04-27 14:02:03 +0500

    added product model

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+
 # Create your models here.
 
 
@@ -18,7 +19,8 @@ class Category(models.Model):
 class Subcategory(models.Model):
     name = models.CharField(verbose_name="Название подкатегории", max_length=150, unique=True)
     slug = models.SlugField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория", related_name="categories")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория",
+                                 related_name="categories")
 
     def __str__(self):
         return self.name
@@ -31,3 +33,35 @@ class Subcategory(models.Model):
     class Meta:
         verbose_name = "Подкатегория"
         verbose_name_plural = "Подкатегории"
+
+
+class Product(models.Model):
+    PRODUCT_TYPES = (
+        ("new", "New"),
+        ("sale", "Sale"),
+        ("sold", "Sold"),
+    )
+
+    title = models.CharField(verbose_name="Название товара", max_length=150, unique=True)
+    descr = models.TextField(verbose_name="Описание товара")
+    price = models.IntegerField(verbose_name="Цена товара")
+    quantity = models.IntegerField(verbose_name="Кол-во товара", default=10)
+    product_type = models.CharField(verbose_name="Тип продукта", choices=PRODUCT_TYPES, max_length=4)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, blank=True, null=True)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductImage(models.Model):
+    photo = models.ImageField(verbose_name="Фото", upload_to="products/", null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+# телефон самсунг telefon-samsung
