0e1cb463b81d73fca20776dfcf7732c89b6c5588
ildar <sayildar17@gmail.com>
2023-05-04 13:55:56 +0500

    added modal and pagination

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import Product, Subcategory
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -10,8 +11,12 @@ def home_view(request):
 
 def shop_view(request):
     products = Product.objects.all()
+    paginator = Paginator(products, 1)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
+
     context = {
-        "products": products
+        "products": result
     }
     return render(request, "pages/shop.html", context)
 
